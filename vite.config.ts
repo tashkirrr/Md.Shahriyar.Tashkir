2688b391445fabcc9e36135312c98e50c9720ad4
Mohammad Shahriyar Tashkir <shahriyar.tashkir@gmail.com>
2026-04-16 13:44:34 +0600

    build(vite): set base path for deployment
    - Change base path to "/Md.Shahriyar.Tashkir/" in vite.config.ts
    - Update asset and script paths in dist/index.html to include base path
    - Ensure proper loading of CSS and JS with the new base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/Md.Shahriyar.Tashkir/",
   server: {
     host: "::",
     port: 8080,
